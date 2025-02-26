9f76eabcd65f18227d123d903c3f405817d226c1
Qchengju <2804033984@qq.com>
2025-02-26 16:57:29 +0800

    feat(recruitment): 更新切换组件样式，增加活动指示器

--- src/components/recruitment/ToggleShow.vue
+++ src/components/recruitment/ToggleShow.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
 import { Button } from "@/components/ui/button";
-
-import { defineProps, ref, defineEmits, watch } from "vue";
+import { defineProps, ref, defineEmits, watch} from "vue";
 
 const emit = defineEmits(["transferToggleShowStatus"]);
 
@@ -16,13 +15,16 @@ const props = defineProps<{
   toggleItems: Item[];
 }>();
 const status = ref<number>(0);
+const activeButton = ref<HTMLElement | null>(null);
 // 切换组件的样式控制
-function toggleActive(index: number) {
+function toggleActive(index: number, event: Event) {
   status.value = index;
   props.toggleItems.forEach((item) => {
     item.isActive = item.index === index;
   });
+  activeButton.value = event.currentTarget as HTMLElement;
 }
+
 
 watch(status, (newValue) => {
   // 发送事件
@@ -35,12 +37,15 @@ watch(status, (newValue) => {
     <MenubarMenu>
       <MenubarTrigger v-for="item in toggleItems" :key="item.index">
         <Button
+          ref="activeButton"
           :class="item.isActive ? 'active' : 'not-active'"
-          @click="toggleActive(item.index)"
+          @click="(event) => toggleActive(item.index, event)"
         >
-          {{ item.title }}</Button
-        >
+          {{ item.title }}
+        </Button>
+        <div v-show="item.isActive" class="active-indicator"></div>
       </MenubarTrigger>
+
     </MenubarMenu>
   </Menubar>
 </template>
@@ -55,15 +60,24 @@ $undertone: #647499;
   flex-direction: row;
   justify-content: flex-start;
   align-items: center;
-  background-color: var(--accent);
+  position: relative;
 }
 .active {
+  color: skyblue;
   background-color: white;
-  color: var(--white);
 }
 .not-active {
-  background-color: var(--accent);
-  color: $undertone;
+  color: black;
   border: none;
+  background-color: white;
 }
+.active-indicator {
+  width: 80px;
+  position: absolute;
+  bottom: 0;
+  left: 10px;
+  height: 4px;
+  background-color: skyblue;
+}
+
 </style>
