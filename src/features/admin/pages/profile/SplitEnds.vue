2bc711d44c4622b4223857fa1af0dd059fea07e9
mq <1596684751@qq.com>
2025-02-26 22:07:29 +0800

    feat:概况中国地图，小组毕业人员分布静态页面

--- src/features/admin/pages/profile/SplitEnds.vue
+++ src/features/admin/pages/profile/SplitEnds.vue
@@ -8,9 +8,6 @@ const data = [
     { name: '后端', total: 20, predicted: 20 },
     { name: '考研', total: 2, predicted: 2 },
     { name: '实习', total: 10, predicted: 10 }
-    // { name: 'Apr', total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
-    // { name: 'May', total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
-    // { name: 'Jun', total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
 ]
 
 const colors = ['#e8cf94', '#58b99d', '#cfd1d3', '#3d79b1']
