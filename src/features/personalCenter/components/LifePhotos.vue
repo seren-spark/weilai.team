08c22b1aec70e7fa1ba7dd7b992234303ae113c9
Qchengju <2804033984@qq.com>
2025-03-11 17:45:57 +0800

    feat: 新增个人中心相关组件和功能，包括图片上传、全局加载动画、API请求封装及样式定义

---
+++ src/features/personalCenter/components/LifePhotos.vue
@@ -0,0 +1,268 @@
+<template>
+  <Teleport v-if="addPhotoLoading" to="body">
+    <GlobalLoading />
+  </Teleport>
+  <div>
+    <div class="title">
+      <p>Life Pictures</p>
+    </div>
+    <div class="images-container">
+      <img
+        v-for="image in images"
+        :key="hash(image)"
+        :src="image"
+        alt=""
+        class="image"
+        :class="{ 'delete-mode': deleteMode, 'selected': selectedImage === image, 'shake': deleteMode }"
+        @click="deleteMode && selectImage(image)"
+      />
+      <div v-if="images.length > 0" class="image-action">
+        <PictureFileUpload class="action-item" @upload:images="imagesHandle" />
+        <div class="action-item delete" @click="toggleDeleteMode">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+          >
+            <path
+              fill="currentColor"
+              fill-rule="evenodd"
+              d="M1.707.293A1 1 0 0 0 .293 1.707L5.586 7L.293 12.293a1 1 0 1 0 1.414 1.414L7 8.414l5.293 5.293a1 1 0 0 0 1.414-1.414L8.414 7l5.293-5.293A1 1 0 0 0 12.293.293L7 5.586z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+      <div v-if="getPhotoListLoading" class="loading">
+        <GlobalLoading />
+      </div>
+      <div v-else-if="images.length === 0" class="no-data">
+        <NewNoData />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NewNoData } from "@/components/recruitment";
+import { onBeforeMount, onMounted, ref, watch } from "vue";
+import { useUserStore } from "@/store/userStore";
+import { useApiRequest, apis } from "./httpClient";
+import type { ApiResponseData } from "@/types/api-response";
+import { hash } from "@/utils/createHashString";
+import GlobalLoading from "@/components/loading/global-loading.vue";
+import PictureFileUpload from "./picture-file-upload.vue";
+import { useAlert } from "@/composables/useAlert";
+const { showAlert } = useAlert();
+import {showConfirm} from "@/composables/useConfirm";
+// 图片展示开始
+const {
+  data: imagesShowData,
+  loading: getPhotoListLoading,
+  error: getPhotoListError,
+  fetchData: getPhotoList,
+} = useApiRequest<ApiResponseData<string[]>>({
+  url: apis.getUserLifePhotos.url.replace(
+    "{userId}",
+    useUserStore().$state.userId.toString(),
+  ),
+  method: apis.getUserLifePhotos.method,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+const images = ref<string[]>([]);
+
+watch([imagesShowData, getPhotoListError], ([newData, newError]) => {
+  if (newData) {
+    images.value = newData.data || [];
+  }
+  if (newError) {
+    showAlert(newError?.message || "获取照片失败", "error");
+  }
+});
+// 图片展示结束
+
+const PhotosData = ref<FormData | null>(null);
+const {
+  data: addPhotoData,
+  error: addPhotoError,
+  loading: addPhotoLoading,
+  fetchData: uploadPhotos,
+} = useApiRequest<ApiResponseData<string>>({
+  url: apis.addUserLifePhotos.url,
+  method: apis.addUserLifePhotos.method,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+  data: PhotosData.value,
+});
+
+const imagesHandle = (files: FileList) => {
+  console.log(files);
+  const formData = new FormData();
+ for (let i = 0; i < files.length; i++) {
+    formData.append("lifePhoto",files[i]);
+  }
+  PhotosData.value = formData;
+  uploadPhotos();
+};
+
+watch([addPhotoData, addPhotoError], ([newData, newError]) => {
+  if (newData?.code === 200) {
+    getPhotoList();
+    console.log(newData);
+    showAlert("上传成功", "pass");
+  }
+  if (newError) {
+    showAlert(newError?.message || "上传失败", "error");
+  }
+});
+
+const deleteMode = ref(false);
+const selectedImage = ref<string | null>(null);
+
+const toggleDeleteMode = () => {
+  deleteMode.value = !deleteMode.value;
+  selectedImage.value = null;
+};
+const {
+  data:deleteData,
+  error:deleteError,
+  fetchData:deletePhoto
+}=useApiRequest<ApiResponseData<string>>({
+  url:apis.deleteUserLifePhotos.url,
+  method:apis.deleteUserLifePhotos.method,
+  headers:{
+    "Content-Type":"application/json",
+  },
+  params:{
+    url:selectedImage.value,
+  },
+});
+
+const selectImage = (image: string) => {
+  selectedImage.value = image;
+  showConfirm({
+    title: "系统提示",
+    content: "确定删除这张照片吗？",
+
+  })
+  // 删除图片
+  .then(() => {
+    deletePhoto();
+  })
+  // 删除图片后，退出删除模式
+  deleteMode.value = false;
+  selectedImage.value = null;
+
+};
+
+watch([deleteData, deleteError], ([newData, newError]) => {
+  console.log(deleteData);
+  if (newData?.code === 200) {
+    getPhotoList();
+    showAlert("删除成功", "pass");
+  }
+  if (newError) {
+    showAlert(newError?.message || "删除失败", "error");
+  }
+});
+
+
+onBeforeMount(() => {
+  console.clear();
+});
+onMounted(() => {
+  getPhotoList();
+});
+</script>
+
+<style lang="scss" scoped>
+@use "@/assets/styles/public.scss";
+.title {
+  width: 100%;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1.5rem 2rem 0 2rem;
+  p {
+    font-size: 1.5rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+    font-style: italic;
+    background-image: linear-gradient(to top, #a8edea 0%, #fed6e3 100%);
+    color: transparent;
+    background-clip: text;
+  }
+}
+.images-container {
+  position: relative;
+  top: 0.5rem;
+  display: flex;
+  min-height: 200px;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin: 0 2rem 2rem 2rem;
+}
+.image {
+  width: 5rem;
+  height: 5rem;
+  border-radius: 5px;
+  object-fit: cover;
+  position: relative;
+  &.delete-mode {
+    cursor: pointer;
+    transition: transform 0.3s ease;
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+  &.selected {
+    border: 3px solid skyblue;
+    box-shadow: 0 0 5px 2px skyblue;
+  }
+  &.shake {
+    animation: shake 1s infinite ease-in;
+    transform-origin: center center;
+  }
+}
+
+@keyframes shake {
+  0% { transform: rotate(0deg); }
+  10% { transform: rotate(-2deg); }
+  20% { transform: rotate(2deg); }
+  30% { transform: rotate(-2deg); }
+  40% { transform: rotate(2deg); }
+  50% { transform: rotate(-2deg); }
+  60% { transform: rotate(2deg); }
+  70% { transform: rotate(-2deg); }
+  80% { transform: rotate(2deg); }
+  90% { transform: rotate(-2deg); }
+  100% { transform: rotate(0deg); }
+}
+.image-action {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 12rem;
+  height: 5rem;
+  position: relative;
+  top: 0;
+  left: 0;
+  gap: 2rem;
+  .action-item {
+    width: 5rem;
+    height: 5rem;
+    background-color: #f5f5f5;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
