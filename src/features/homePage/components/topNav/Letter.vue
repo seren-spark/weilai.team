6fc5e4d4bc1d049e57587c06c6d5c05b627af828
hann <2767073256@qq.com>
2025-04-17 08:18:35 +0800

    首页导航栏

---
+++ src/features/homePage/components/topNav/Letter.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import Button from "@/components/ui/button/Button.vue";
+</script>
+
+<template>
+  <div class="titleCon">
+    <div>
+      <div class="title">未来软件工作室</div>
+      <div class="enTitle">Future software Studio</div>
+      <div class="title_person">做一个有影响力的人</div>
+      <div class="title_team">做一个有影响力的团队</div>
+      <Button class="more">了解更多</Button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.titleCon {
+  width: 70%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  z-index: 4;
+  top: 0;
+  left: 0;
+  margin-top: 15rem;
+  .title {
+    color: white;
+    font-size: 63px;
+    font-weight: bold;
+  }
+  .enTitle {
+    font-size: 32px;
+    font-weight: bold;
+    margin-bottom: 1rem;
+    background: linear-gradient(45deg, #74b0ce, #60d9c9, #868dd2, #aa00ff);
+    background-clip: text;
+    background-size: 150%;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: color-change 4s linear infinite;
+  }
+  @keyframes color-change {
+    0% {
+      background-position: 0;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0 50%;
+    }
+  }
+  .title_person {
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+    margin-bottom: 1rem;
+  }
+  .title_team {
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+    margin-bottom: 5rem;
+  }
+  .more {
+    width: 12.5rem;
+    height: 3rem;
+    font-size: 1.2rem;
+    color: white;
+    font-weight: bold;
+    color: #284dd5;
+    border: 0.16rem solid #284dd5;
+    background-color: transparent;
+    position: relative;
+    z-index: 1;
+    transition: 2s;
+    overflow: hidden;
+  }
+  .more::before,
+  .more::after {
+    content: "";
+    position: absolute;
+    top: -1.2rem;
+    z-index: -1;
+    width: 170%;
+    aspect-ratio: 1;
+    border: none;
+    border-radius: 40%;
+    background-color: rgba(0, 0, 255, 0.25);
+    transition: 2s;
+  }
+  .more::before {
+    left: -60%;
+    transform: translate3d(0, 6em, 0) rotate(-340deg);
+  }
+  .more::after {
+    right: -60%;
+    transform: translate3d(0, 6em, 0) rotate(390deg);
+  }
+  .more:hover,
+  .more:focus {
+    color: white;
+  }
+  .more:hover::before,
+  .more:hover::after,
+  .more:focus::before,
+  .more:focus::after {
+    transform: none;
+    background-color: rgba(0, 0, 255, 0.75);
+  }
+}
+</style>
---
+++ src/features/homePage/components/topNav/Swiper.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="swiperbox">
+    <Letter />
+    <swiper
+      :slides-per-view="1"
+      :space-between="0"
+      :loop="true"
+      :centered-slides="true"
+      :pagination="{
+        clickable: true,
+        renderBullet: (index, className) => {
+          return `<span class='${className}'></span>`;
+        },
+      }"
+      :autoplay="{ delay: 7000, disableOnInteraction: false }"
+      :navigation="{
+        nextEl: '.custom-next',
+        prevEl: '.custom-prev',
+      }"
+      :modules="modules"
+    >
+      <!-- 幻灯片内容 -->
+      <div class="custom-navigation">
+        <div class="custom-prev">
+          <div class="blocks"></div>
+          <Icon
+            icon="meteor-icons:chevron-left"
+            width="21"
+            height="21"
+            style="color: #fff"
+            class="arrow"
+          />
+        </div>
+        <div class="custom-next">
+          <Icon
+            icon="meteor-icons:chevron-right"
+            width="21"
+            height="21"
+            style="color: #fff"
+            class="arrow"
+          />
+          <div class="blocks"></div>
+        </div>
+      </div>
+      <swiper-slide
+        ><img
+          src="../../../../assets/img/homePage/bg1.jpg"
+          alt=""
+          width="100%"
+          height="100%"
+      /></swiper-slide>
+      <swiper-slide
+        ><img
+          src="../../../../assets/img/homePage/bg2.jpg"
+          alt=""
+          width="100%"
+          height="100%"
+      /></swiper-slide>
+      <swiper-slide
+        ><img
+          src="../../../../assets/img/homePage/bg3.jpg"
+          alt=""
+          width="100%"
+          height="100%"
+      /></swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script setup>
+import { Swiper, SwiperSlide } from "swiper/vue"; // swiper 所需组件
+import { Autoplay, Navigation, Pagination, A11y } from "swiper/modules"; // 分页器
+// import 'swiper/swiper-bundle.css'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Letter from "@/features/homePage/components/topNav/Letter.vue";
+import { Icon } from "@iconify/vue/dist/iconify.js";
+
+const modules = [Autoplay, Pagination, Navigation, A11y];
+</script>
+
+<style lang="scss">
+.swiperbox {
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  z-index: 5;
+
+  .swiper {
+    height: 100vh;
+    width: 100%;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    /* 自定义长条分页器 */
+    .swiper-pagination {
+      position: absolute;
+      bottom: 80px;
+      display: flex;
+      justify-content: center;
+      gap: 8px;
+    }
+
+    .swiper-pagination-bullet {
+      width: 40px; // 长条宽度
+      height: 2.5px; // 长条高度
+      border-radius: 5px;
+      background: rgba(255, 255, 255, 0.5);
+      opacity: 1;
+      transition: all 0.3s;
+    }
+
+    .swiper-pagination-bullet-active {
+      background: white;
+      width: 60px; // 激活状态更长
+    }
+
+    /* 自定义导航箭头 */
+    .custom-navigation {
+      position: absolute;
+      top: 50%;
+      width: 100%;
+      transform: translateY(-50%);
+      z-index: 11;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 20px;
+      pointer-events: none;
+    }
+    .arrow {
+      opacity: 0;
+    }
+    .custom-prev,
+    .custom-next {
+      width: 40px;
+      height: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      pointer-events: auto;
+      .blocks {
+        background-color: white;
+        width: 2px;
+        height: 40px;
+        transition:
+        //   width 0.1s ease,
+          height 0.1s ease;
+      }
+    }
+    .custom-prev:hover .blocks,
+    .custom-next:hover .blocks {
+      width: 0.5px;
+      height: 100px;
+    }
+    .custom-next:hover .arrow {
+      opacity: 1;
+    }
+    .custom-prev:hover .arrow {
+      opacity: 1;
+    }
+  }
+}
+</style>
