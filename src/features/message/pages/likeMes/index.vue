efc1e6d73661d480fb12be928ecc9a59949e875c
hann <2767073256@qq.com>
2025-06-07 18:21:18 +0800

    压缩首页轮播图

--- src/features/message/pages/likeMes/index.vue
+++ src/features/message/pages/likeMes/index.vue
@@ -28,7 +28,7 @@
             :key="message.messageId"
             class="mess"
           >
-            <MesItem :message="message" @like="run()" />
+            <MesItem :message="message" @like="handleLikeEvent" />
           </div>
           <div v-if="loadingMore" class="loading-more">
             <div class="flex items-center space-x-4">
@@ -84,23 +84,40 @@ onMounted(() => {
       messageStore.setLikeStatus(true);
     }
   });
-  run();
+  fetchMessages(false);
   window.addEventListener("scroll", handleScroll);
 });
 
 onUnmounted(() => {
   window.removeEventListener("scroll", handleScroll);
 });
+const handleLikeEvent = (resetPage: boolean) => {
+  if (resetPage) {
+    pageNumber.value = 1;
+    isOver.value = false;
+    messages.value = []; // 清空当前数据
+  }
+  fetchMessages(resetPage);
+};
 
 //渲染消息列表
-const messageList = () => {
+const run = (resetPage = false) => {
+  if (resetPage) {
+    pageNumber.value = 1;
+    isOver.value = false;
+  }
   return apiClient.get(
     `/message/getMessageInfo?messageType=${messageType}&pageSize=${pageSize}&pageNumber=${pageNumber.value}`,
   );
 };
 
-const { data, loading, run } = useRequest(messageList, {
+const {
+  data,
+  loading,
+  run: fetchMessages,
+} = useRequest(run, {
   loadingKeep: 650,
+  manual: true,
 });
 
 watch(
@@ -147,7 +164,7 @@ const handleScroll = async () => {
   if (scrollTop + clientHeight >= scrollHeight - 100) {
     loadingMore.value = true;
     pageNumber.value++;
-    run();
+    fetchMessages();
   }
 };
 
@@ -177,7 +194,7 @@ watch(
       showAlert("删除成功", "pass");
       pageNumber.value = 1;
       isOver.value = false;
-      run();
+      fetchMessages();
     } else {
       showAlert("删除失败", "error");
     }
