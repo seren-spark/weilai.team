1d7c7c2d0da40c71dda5b6141819ac54b2cc667f
wdlxxx <2206655687@qq.com>
2025-06-07 11:33:40 +0800

    fix:修复查看他人的个人中心时的按钮显示bug

--- src/features/personalCenter/components/MyTabs.vue
+++ src/features/personalCenter/components/MyTabs.vue
@@ -1,11 +1,20 @@
 <template>
   <Tabs default-value="posts" class="mt-4">
-    <TabsList class="grid w-full grid-cols-4">
+   <template v-if="!userStore.isSelf">
+     <TabsList class="grid w-full grid-cols-3">
+      <TabsTrigger value="posts"> 原创 </TabsTrigger>
+      <TabsTrigger value="schedule"> 课表 </TabsTrigger>
+      <TabsTrigger value="photos"> 生活照片 </TabsTrigger>
+    </TabsList>
+   </template>
+   <template v-else>
+     <TabsList class="grid w-full grid-cols-4">
       <TabsTrigger value="posts"> 原创 </TabsTrigger>
       <TabsTrigger value="schedule"> 课表 </TabsTrigger>
       <TabsTrigger value="collections"> 收藏 </TabsTrigger>
       <TabsTrigger value="photos"> 生活照片 </TabsTrigger>
     </TabsList>
+   </template>
     <TabsContent value="posts">
       <Posts></Posts>
     </TabsContent>
@@ -27,5 +36,8 @@ import Posts from "./Posts.vue";
 import Schedule from "./Schedule.vue";
 import Collections from "./Collections.vue";
 import LifePhotos from "./LifePhotos.vue";
+
+import { useUserStore } from "@/store/userStore";
+const userStore = useUserStore();
 </script>
 <style lang="scss" scoped></style>
