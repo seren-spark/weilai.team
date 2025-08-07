7a2c060889c1167f3ed79e5c1d1addb333b1957a
mq <1596684751@qq.com>
2025-08-07 15:33:15 +0800

    feat:报名表小问题修改，删除多余文件

--- src/pages/application.vue
+++ src/pages/application.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import ApplicationFrom from "../components/application/ApplicationFrom.vue";
-// import ApplicationTwo from "@/components/application/ApplicationTwo.vue";
 </script>
 
 <template>
   <ApplicationFrom></ApplicationFrom>
-  <!-- <ApplicationTwo></ApplicationTwo> -->
 </template>
 
 <style lang="scss" scoped></style>
