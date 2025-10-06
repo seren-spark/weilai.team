e3b1750287cf5d58771d1036087f39eb124955ca
hann <2767073256@qq.com>
2025-10-06 11:39:24 +0800

    feat：取消跳转报名页

--- src/features/homePage/components/animation/cute.vue
+++ src/features/homePage/components/animation/cute.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="content" @mouseenter="stopAnimation" @mouseleave="startAnimation">
     <div class="moving-container">
-      <RouterLink to="/application">
-        <img
-          ref="movingImg"
-          src="../../../../assets/img/homePage/Animation1.gif"
-          alt="图片"
-          class="moving-image"
-        />
-      </RouterLink>
-      <div v-show="isEnterShow" class="text">
+      <!-- 招新时取消注释跳转报名页 -->
+      <!-- <RouterLink to="/application"> -->
+      <img
+        ref="movingImg"
+        src="../../../../assets/img/homePage/Animation1.gif"
+        alt="图片"
+        class="moving-image"
+      />
+      <!-- </RouterLink> -->
+      <!-- <div v-show="isEnterShow" class="text">
         <img src="../../../../assets/img/homePage/enter.png" alt="报名" />
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
